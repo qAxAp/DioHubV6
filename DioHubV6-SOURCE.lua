dbf2f8fe1c3fc8c6e5ff8390781d6ba0ab755f80
AxA <162728663+qAxAp@users.noreply.github.com>
2024-12-11 20:13:17 +0100

    Create DioHubV6-SOURCE.lua

---
+++ DioHubV6-SOURCE.lua
@@ -0,0 +1,102 @@
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/qAxAp/Test./refs/heads/main/Rocky.lua'))()
+
+--[[ Here is the main title for the Script. Above is the UI's Library, which is the looks of the UI ]]--
+
+local Window = Library:CreateWindow("DioHub", "V6", 10044538000)
+
+-- [[ You can Modify The tabs here ]]--
+local Tab = Window:CreateTab("Scripts 📃")
+local Page1 = Tab:CreateFrame("Murder mystery ")
+local Page2 = Tab:CreateFrame("Arsenal ")
+local Page3 = Tab:CreateFrame("Muscle legends")
+local Page4 = Tab:CreateFrame("Blox Fruits ")
+local Page5 = Tab:CreateFrame("Prison Life ")
+local Page6 = Tab:CreateFrame("BedWars (coming soon..) ")
+local Page7 = Tab:CreateFrame("TRD - Camp ")
+local Page8 = Tab:CreateFrame("Legends of Speed ")
+local Page9 = Tab:CreateFrame("Da Hood ")
+local Page10 = Tab:CreateFrame("Brookhaven ")
+local Page11 = Tab:CreateFrame("Universal scripts ")
+
+--[[ Modify the amount of Buttons here and replace the loadstring for whichever script you want to have. ]]--
+
+local Button1_Page1 = Page1:CreateButton("Murder mystery Hub", "Very old", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Murder%20Mystery"))()
+end)
+
+local Button2_Page1 = Page1:CreateButton("Maids hub", "old + unstable", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1andonlymars/MarsHub/main/MM2"))()
+end)
+
+local Button2_Page2 = Page2:CreateButton("DioHub Aimbot V2 ", "Aimbot", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/qAxAp/Arsenal-Aimbot/refs/heads/main/Aimbot.lua'))()
+end)
+
+local Button4_Page2 = Page2:CreateButton("Extended Hitbox", "Voidy", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidzyScripter/VoidHub-/main/Script"))()
+end)
+
+local Button1_Page2 = Page2:CreateButton("DioHub Aimbot V1", "Aimbot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qAxAp/Arsenal/main/Protected_9362614235196757.lua"))()
+end)
+
+local Button3_Page2 = Page2:CreateButton("DioHub Aimbot V3", "Aimbot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qAxAp/Arsenal-Aimbot/refs/heads/main/Untitled%205.lua"))()
+end)
+
+local Button2_Page3 = Page3:CreateButton("Auto Farm", "Auto Farm", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/harisiskandar178/Roblox-Script/main/Muscle%20Legend"))()
+end)
+
+local Button1_Page4 = Page4:CreateButton("Redz Hub (Blox Fruits)", "Redz Hub ( OP )", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+end)
+
+local Button2_Page4 = Page4:CreateButton("W azure", "Submitted by Menuchi the W", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qAxAp/Not-my-script/main/BloxFruits"))()
+end)
+
+local Button1_Page5 = Page5:CreateButton("Tbao Hub", "Tbao Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/thaibao/main/TbaohubPrisonLife"))()
+end)
+
+local Button1_Page7 = Page7:CreateButton("Vyra", "Vyra - Camp Script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kureu/Roblox-T.R.D-Camp-Script/main/Roblox%20T.R.D%20Camp%20Script"))()
+end)
+
+local Button1_Page8 = Page8:CreateButton("Legends of Speed V2", "Legends of Speed V2", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KrangH/ScriptsHub/main/Legends_Of_Speedv2"))()
+end)
+
+local Button1_Page9 = Page9:CreateButton("Swag Mode", "Swag Mode", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002"))()
+end)
+
+local Button1_Page10 = Page10:CreateButton("SanderX", "SamderX", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sXPiterXs1111/SanderXNewUpdate3.1.lua/main/SanderXNewUpdate.lua"))()
+end)
+
+local Button1_Page11 = Page11:CreateButton("Infinite Yield", "Infinite Yield", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+local Button2_Page11 = Page11:CreateButton("Fly GUI v3", "Fly GUI v3", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+end)
+
+local Button3_Page11 = Page11:CreateButton("Nameless Admin", "Nameless Admin V1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
+end)
+
+local Button4_Page11 = Page11:CreateButton("Synapse X", "Synapse X Executor", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Chillz-s-scripts/main/Synapse-X-Remake.lua"))()
+end)
+
+local Button5_Page11 = Page11:CreateButton("ESP", "DioHubs Simple ESP", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qAxAp/Zuckerberg/refs/heads/main/Untitled%209.lua"))()
+end)
+
+
+local Button6_Page11 = Page11:CreateButton("DioFly", "DioFly", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qAxAp/DioFly/main/DioFly.lua"))()
+end)
